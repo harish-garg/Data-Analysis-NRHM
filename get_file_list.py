3c5a351cf155368571e6793c1aca5b514a07f63c
Harish Garg <harish.garg@gmail.com>
2017-03-10 11:40:54 +0530

    feat: Added more file recursive code and also more progress on the dataframe cleanup.

--- get_file_list.py
+++ get_file_list.py
@@ -1,22 +1,30 @@
-# Import modules
-import os
+#!/usr/bin/python
 
+# Import the necessary modules
+import os
+import pandas as pd
+
+# Dataset location
 data_dir = 'DHIS (APRIL 15-MARCH 16)'
 
 # Retrieve current working directory (`cwd`)
 cwd = os.getcwd()
-print "We are here..." + cwd
+print "\n......\nWe are here..." + cwd + "\n......\n"
 
 # get the paths for all the xls files in a list
 file_list = []
-
 for root, dirs, files in os.walk(data_dir, topdown=False):
     for name in files:
-        file_path = os.path.join(root, name)
-        print(file_path)
-        file_list.append(file_path)
-    #for name in dirs:
-        #print(os.path.join(root, name))
-
+        file_list.append([os.path.join(root, name), root, name])
+    
 #print(file_list)
-print(len(file_list))
+print('No of data files - ', str(len(file_list)))
+
+# import the files into dataframes
+dataframes = []
+
+for f in file_list:
+    print(f[0])
+    dataframes.append(pd.read_excel(f[0]))
+    
+print(dataframes[0].head())
